afb7d23e1920dac2a4269b406e657aad32d81e8b
Focus2334 <danilakurzuev@gmail.com>
2025-11-27 15:56:02 +0500

    add backend

---
+++ Matmekh.Maps/Matmekh.Maps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Matmekh.Maps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
