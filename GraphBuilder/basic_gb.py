dc2ec15262a2f0ef3b8f5330c8c95e2e12f9f6e4
Alexei Zhirnov <alexeizhirnov06@gmail.com>
2025-12-03 21:45:54 +0500

    polygon start

--- GraphBuilder/basic_gb.py
+++ GraphBuilder/basic_gb.py
@@ -16,7 +16,9 @@ class GraphBuilder:
 
     def run(self):
         self._process_floor_plan()
-        self._visualize_results()
+        self._split_into_polygons()
+        # self._visualize_results()
+        self._visualize_heatmap()
 
         with open(f"{self.image_path[:-4]}_contour_out.txt", "w") as f:
             f.write(str(self.contour_all))
@@ -85,29 +87,83 @@ class GraphBuilder:
             cv2.circle(result_img, point, 0, color, 0)
         return result_img
 
+    def _make_cardinal_axis_line(self, start_x, start_y, finish, is_x_dir: bool):
+        # start: [0]x, [1]y
+        # polygon_field: [0]y, [1]x
+
+        axis = 0 if is_x_dir else 1
+        indices = [start_y, start_x]
+        # indices = [start_x, start_y]
+        for i in range(start_x if is_x_dir else start_y, finish):
+            indices[1 - axis] = i
+            if self.polygon_field[tuple(indices)] > 0:
+                self.polygon_field[tuple(indices)] += 1
+            else:
+                break
+
     def _split_into_polygons(self):
         c_len = len(self.contour_compressed)
-        for i in range(c_len):
-            line_coeffs: Tuple[int, int] = None
+        height, width = self.binary_mask.shape
+        self.polygon_field = self.binary_mask.copy()
+        # self.polygon_field[2,5] = '52' (2 по y, 5 по x)
+        for p in range(c_len):
+            # line_coeffs: Tuple[int, int] = None
+            a, b = (
+                self.contour_compressed[p],
+                self.contour_compressed[(p + 1) % c_len],
+            )  # а здесь [0] - по x, [1] - по y
+            difx = a[0] - b[0]
+            dify = a[1] - b[1]
 
-            """
-            A(x1, y1) B(x2, y2) : ax + by + c = 0
-            1. находим a,b,c
-            2. проводим прямую и смотрим для каждой последовательной пары точек, есть ли пересечение с contour
-                (!) а если прямая очень неровная, то будем смотреть окрестность?
-                    наверно нет, стоит просто понять какой конфигурацией алгоритма построения прямой мы это делаем
-                    и применить именно его. должны очев получить ту же самую прямую
-                если нашлось, берём самое ближнее:
-                    если получились точки с обеих сторон отрезка AB, то берём обе самые ближние точки к A и B соотв.
-                        хотя могло получиться что и больше точек нужно...
-                иначе ничего
-            получили список точек, их добавляем на картинку наравне с corner_points (которые получились из appox_simple)
+            if difx != 0 and dify != 0:
+                # raise RuntimeError("Диагональ!!! аааа")
+                # print(f"непрямая на {a}, {b}!!!")
+                if abs(a[0]-b[0]) > 1 and abs(a[1]-b[1]) > 1:
+                    print(f"нетривиальная непрямая на {a},{b}")
+                
 
-            """
+            if dify != 0:  # прямая по y
+                start = a if a[1] < b[1] else b
+                self._make_cardinal_axis_line(
+                    start_x=start[0], start_y=start[1], finish=height, is_x_dir=False
+                )
+                self._make_cardinal_axis_line(
+                    start_x=start[0], start_y=0, finish=start[1], is_x_dir=False
+                )
+
+            elif difx != 0:  # прямая по x
+                start = a if a[0] < b[0] else b
+                self._make_cardinal_axis_line(
+                    start_x=start[0], start_y=start[1], finish=width, is_x_dir=True
+                )
+                self._make_cardinal_axis_line(
+                    start_x=0, start_y=start[1], finish=start[0], is_x_dir=True
+                )
+            else:
+                print(f"poing doubling: {a}")
+        # print("aboba")
+
+        """
+        (!!!) UPDATE: непрямые линии не сохраняются в виде уравнения, 
+        а в виде горизонтальных / вертикальных отрезочков (!!!)
+
+        A(x1, y1) B(x2, y2) : ax + by + c = 0
+        1. находим a,b,c
+        2. проводим прямую и смотрим для каждой последовательной пары точек, есть ли пересечение с contour
+            (!) а если прямая очень неровная, то будем смотреть окрестность?
+                наверно нет, стоит просто понять какой конфигурацией алгоритма построения прямой мы это делаем
+                и применить именно его. должны очев получить ту же самую прямую
+            если нашлось, берём самое ближнее:
+                если получились точки с обеих сторон отрезка AB, то берём обе самые ближние точки к A и B соотв.
+                    хотя могло получиться что и больше точек нужно...
+            иначе ничего
+        получили список точек, их добавляем на картинку наравне с corner_points (которые получились из appox_simple)
+
+        """
 
     def _visualize_results(self):
 
-        fig, axes = plt.subplots(2, 3, figsize=(18, 12))
+        fig, axes = plt.subplots(2, 3, figsize=(36, 24))
         axes[0, 0].imshow(self.image)
         axes[0, 0].set_title("Исходное изображение")
 
@@ -130,7 +186,31 @@ class GraphBuilder:
         )
         axes[1, 0].set_title("Контур коридора")
 
+        # 'hot', 'coolwarm', 'plasma', 'inferno'
+        axes[1, 1].imshow(self.polygon_field, cmap="viridis")
+        # axes[1, 1].set_colorbar(label="Значения")
+        axes[1, 1].set_title("Heatmap")
+
         plt.tight_layout()
+        plt.show()
+
+    def _visualize_heatmap(self):
+        plt.figure(figsize=(800,800))
+        plt.imshow(
+            GraphBuilder._draw_contour_points_over_image(
+                GraphBuilder._draw_contour_points_over_image(
+                    self.image, self.contour_all, True
+                ),
+                self.contour_compressed,
+                False,
+            )
+        )
+
+
+        # 'hot', 'coolwarm', 'plasma', 'inferno'
+        plt.imshow(self.polygon_field, cmap="viridis")
+        # plt.set_colorbar(label="Значения")
+        plt.title("Heatmap")
         plt.show()
 
 
@@ -207,4 +287,5 @@ def run_imgprep():
 
 
 if __name__ == "__main__":
-    run_imgprep()
+    # run_imgprep()
+    run_gb()
