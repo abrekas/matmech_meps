9f4df2eb982acf90b30a480d6253628a96a7ec97
Ilona Kazantseva <Ilona.Kazantseva@urfu.me>
2025-12-07 11:36:48 +0500

    шторка и переходы по страницам

--- Matmekh.Maps/Matmekh.Maps/Infrastructure/wwwroot/js/map_script.js
+++ Matmekh.Maps/Matmekh.Maps/Infrastructure/wwwroot/js/map_script.js
@@ -1,3 +1,20 @@
+function activateMenu(e) {
+    e.stopPropagation();
+
+    const menu = document.getElementById('myMenu');
+    menu.classList.toggle('active');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menu = document.getElementById('myMenu');
+    const panel = document.querySelector('.menu-panel');
+    panel.addEventListener('click', function(e) {
+        e.stopPropagation();
+    });
+    menu.addEventListener('click', function() {
+        menu.classList.remove('active');
+    });
+});
 document.addEventListener("DOMContentLoaded", () => {
     const floorSelect = document.getElementById("floorSelect");
     const floorLabel  = document.getElementById("floorLabel");
