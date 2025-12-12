7063f87fb43832696cc8a4d84db9e96828e3b1c6
Ilona Kazantseva <Ilona.Kazantseva@urfu.me>
2025-12-12 11:21:29 +0500

    логика страниц построения маршрута и кнопка закрытия построения маршрута

--- Matmekh.Maps/Matmekh.Maps/Infrastructure/wwwroot/js/route.js
+++ Matmekh.Maps/Matmekh.Maps/Infrastructure/wwwroot/js/route.js
@@ -1,12 +1,29 @@
-const goRouteBtn = document.getElementById("goRoute");
-const closeRouteBtn = document.getElementById("closeRoute");
-if (goRouteBtn) {
-goRouteBtn.addEventListener("click", () => {
-    window.location.href = "matmeh_route_page.html";
+document.addEventListener("DOMContentLoaded", () => {
+    const goRouteKuibyshevaBtn = document.getElementById("goRouteK");
+    const closeRouteKuibyshevaBtn = document.getElementById("closeRouteK");
+    const goRouteMatMexBtn = document.getElementById("goRouteM");
+    const closeRouteMatMexBtn = document.getElementById("closeRouteM");
+    if (goRouteKuibyshevaBtn) {
+        goRouteKuibyshevaBtn.addEventListener("click", () => {
+            window.location.href = "kuibysheva_route_page.html";
+        });
+    }
+
+    if (closeRouteKuibyshevaBtn) {
+        closeRouteKuibyshevaBtn.addEventListener("click", () => {
+            window.location.href = "kuibysheva_start_page.html";
+        });
+    }
+
+    if (goRouteMatMexBtn) {
+        goRouteMatMexBtn.addEventListener("click", () => {
+            window.location.href = "matmeh_route_page.html";
+        });
+    }
+
+    if (closeRouteMatMexBtn) {
+        closeRouteMatMexBtn.addEventListener("click", () => {
+            window.location.href = "matmeh_start_page.html";
+        });
+    }
 });
-}
-if (closeRouteBtn) {
-closeRouteBtn.addEventListener("click", () => {
-    window.location.href = "index.html";
-});
-}
